6e5db6f15e0624640e6eec0fb41d40671aa3864e
hacksoi <sohackinp@gmail.com>
2018-04-25 04:09:38 -0700

    started working on http server

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #if 0
-TODO:
-    -websockets.h
-        -alert client websocket thread about closure from background thread
 #endif
 
 #include "ns_common.h"
 #include "ns_message_queue.h"
-#include "ns_socket_wrapper.h"
-#include "ns_websockets.h"
-#include "ns_files.h"
+#include "ns_socket.h"
+#include "ns_websocket.h"
+#include "ns_file.h"
+#include "ns_http_server.h"
 
 #include <assert.h>
 #include <stdio.h>
@@ -34,23 +32,24 @@ TODO:
 #define VIDEO_STREAM_PORT "3491"
 
 
-// TODO: sync this
-pthread_t thread_pool[20];
-int thread_pool_size;
+NsThreadPool thread_pool;
 
 
-void *websocket_client_thread_entry(void *thread_data)
+void *
+video_stream_client_thread_entry(void *thread_data)
 {
     NsWebSocket *websocket = (NsWebSocket *)thread_data;
 
 #if 1
-    // perform basic test
     {
+        printf("performing basic test...\n");
+
         char test_message[256];
         int message_length = ns_websocket_receive(websocket, test_message, sizeof(test_message));
-        if(message_length == NS_ERROR)
+        if(message_length <= 0)
         {
-            exit(1);
+            DebugPrintInfo();
+            return (void *)NS_ERROR;
         }
 
         printf("test message: %s\n", test_message);
@@ -58,57 +57,49 @@ void *websocket_client_thread_entry(void *thread_data)
         if(strcmp(test_message, "this is a test"))
         {
             DebugPrintInfo();
-            exit(1);
+            return (void *)NS_ERROR;
         }
 
         strcat(test_message, " - received!");
-        if(ns_websocket_send(websocket, test_message, strlen(test_message)) == NS_ERROR)
+        int bytes_sent = ns_websocket_send(websocket, test_message, strlen(test_message));
+        if(bytes_sent <= 0)
         {
-            exit(1);
+            DebugPrintInfo();
+            return (void *)NS_ERROR;
         }
-    }
 
-    printf("basic test passed\n");
+        printf("basic test passed\n");
+    }
 #endif
 
-#if 1
     while(1)
     {
-#if 0
-        uint8_t message[256];
-        int message_length = recv(socket, message);
-        if(message_length == -1)
-        {
-            exit(1);
-        }
-        message[message_length] = 0;
-
-        printf("socket message received (%d bytes): %s\n", strlen((char *)message), (char *)message);
-
-        strcat((char *)message, " - received!");
-        if(!send(socket, message, strlen((char *)message)))
-        {
-            exit(1);
-        }
-#endif
-
         char *message = (char *)"funny cat photos";
-        if(ns_websocket_send(websocket, message, (strlen(message) + 1)) == NS_ERROR)
+        int bytes_sent = ns_websocket_send(websocket, message, (strlen(message) + 1));
+        if(bytes_sent == NS_WEBSOCKET_CLIENT_CLOSED)
         {
-            exit(1);
+            printf("closing thread...\n");
+            return (void *)NS_SUCCESS;
+        }
+        else if(bytes_sent <= 0)
+        {
+            DebugPrintInfo();
+            return (void *)bytes_sent;
         }
 
         usleep(500000);
     }
-#endif
 
     return 0;
 }
 
-void *stream_thread_entry(void *data)
+void *
+video_stream_client_getter_thread_entry(void *data)
 {
+    int status;
     NsWebSocket websocket;
-    if(ns_websocket_create(&websocket, VIDEO_STREAM_PORT) == NS_ERROR)
+
+    if(ns_websocket_create(&websocket, VIDEO_STREAM_PORT) != NS_SUCCESS)
     {
         DebugPrintInfo();
         exit(1);
@@ -118,61 +109,68 @@ void *stream_thread_entry(void *data)
     int num_clients = 0;
     while(1)
     {
-        ns_websocket_get_client(&websocket, &clients[num_clients]);
-        pthread_create(&thread_pool[thread_pool_size++], NULL, websocket_client_thread_entry, &clients[num_clients++]);
+        status = ns_websocket_get_client(&websocket, &clients[num_clients]);
+        if(status != NS_SUCCESS)
+        {
+            DebugPrintInfo();
+            exit(1);
+        }
+
+        NsThread *client_thread;
+        status = ns_thread_pool_get(&thread_pool, &client_thread);
+        if(status != NS_SUCCESS)
+        {
+            DebugPrintInfo();
+            exit(1);
+        }
+
+        status = ns_thread_pool_create_thread(&thread_pool, video_stream_client_thread_entry, 
+                                              &clients[num_clients++]);
+        if(status != NS_SUCCESS)
+        {
+            DebugPrintInfo();
+            exit(1);
+        }
     }
 
     return 0;
 }
 
-int main(void)
+int 
+main(void)
 {
-    ns_sockets_init();
+    int status;
 
-    pthread_create(&thread_pool[thread_pool_size++], NULL, stream_thread_entry, NULL);
-
-    NsSocket socket;
-    if(ns_socket_create(&socket, WEBSERVER_PORT) == NS_ERROR)
+    status = ns_websockets_init();
+    if(status != NS_SUCCESS)
     {
+        DebugPrintInfo();
         exit(1);
     }
 
-    char webpage[Kilobytes(4)];
-    int webpage_size;
+    status = ns_thread_pool_create(&thread_pool, 256);
+    if(status != NS_SUCCESS)
     {
-        NsFile file;
-        ns_file_open(&file, (char *)"webpage.html");
-
-        webpage_size = ns_file_load(&file, webpage, sizeof(webpage));
-        if(webpage_size == NS_ERROR)
-        {
-            exit(1);
-        }
-
-        if(ns_file_close(&file) == NS_ERROR)
-        {
-            exit(1);
-        }
+        DebugPrintInfo();
+        exit(1);
     }
 
-	printf("server: waiting for connections...\n");
-
-    while(1)
+    status = ns_thread_pool_create_thread(&thread_pool, video_stream_client_getter_thread_entry, NULL);
+    if(status != NS_SUCCESS)
     {
-        NsSocket client_socket;
-        if(ns_socket_get_client(&socket, &client_socket) == NS_ERROR)
+        DebugPrintInfo();
+        exit(1);
+    }
+
+    NsHttpServer http_server;
+    ns_http_server_create(&http_server);
+
+    for(;;)
+    {
+        if(pause() == -1)
         {
             exit(1);
         }
-
-        if(ns_socket_send(&client_socket, webpage, webpage_size) == NS_ERROR) 
-        {
-            exit(1);
-        }
-
-        // for some reason, closing it right after causes the browser to not receive our html. shutdown() fixes that.
-        ns_socket_shutdown(&client_socket, NS_SOCKET_RDWR);
-        ns_socket_close(&client_socket);
     }
 
 	return 0;
